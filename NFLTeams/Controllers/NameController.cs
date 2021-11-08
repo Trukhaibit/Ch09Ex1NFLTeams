012c7f18f86b8dd63ec209baaa8318b49234eb0f
James Olszewski <lil.oski.one@gmail.com>
2021-11-07 20:57:07 -0500

    Add project files.

---
+++ NFLTeams/Controllers/NameController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NFLTeams.Models;
+
+namespace NFLTeams.Controllers
+{
+    [HttpGet]
+    public ViewResult Index()
+    {
+        var session = new NFLSession(HttpContext.Session);
+        var model = new TeamListViewModel
+        {
+            ActiveConf = session.GetActiveConf(),
+            ActiveDiv = session.GetActiveDiv(),
+            Teams = session.GetMyTeams(),
+            UserName = session.GetName()
+        };
+
+        return View(model);
+
+    }
+    [HttpPost]
+    public RedirectToActionResult Change(TeamListViewModel model)
+    {
+        var session = new NFLSession(HttpContext.Session); session.SetName(model.UserName);
+        return RedirectToAction("Index", "Home", new
+        {
+            ActiveConf = session.GetActiveConf(),
+            ActiveDiv = session.GetActiveDiv()
+        });
+    }
+}
